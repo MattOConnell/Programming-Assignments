004f1b67e4b2ee6c91ba0a3d63914ed707123724
MattOConnell <oaks1241@gmail.com>
2013-09-23 16:15:44 -0400

    Answer to Question 1 on Assignment 1

---
+++ Question-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+
+    cout << "Enter #1: ";
+    cin >> a;
+    cout << "Enter #2: ";
+    cin >> b;
+    cout << "Enter #3: ";
+    cin >> c;
+    cout << "Enter #4: ";
+    cin >> d;
+    cout << "Enter #5: ";
+    cin >> e;
+    cout << "Sum: " << a + c + b + d + e;
+    return 0;
+}
